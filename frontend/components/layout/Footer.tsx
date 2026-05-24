8b494baff6d9f6911041b70b7479f14ac90370cd
hamzayerr <hamzayerrou62@gmail.com>
2026-05-24 20:22:20 +0200

    Footer accordion: move titles to left side, chevrons to right

--- frontend/components/layout/Footer.tsx
+++ frontend/components/layout/Footer.tsx
@@ -39,10 +39,10 @@ function AccordionSection({ title, links }: { title: string; links: { href: stri
         onClick={() => setOpen(o => !o)}
         className="w-full flex items-center justify-between py-4"
       >
+        <h4 className="font-cairo font-bold text-brand-900">{title}</h4>
         <ChevronDown
           className={cn('w-5 h-5 text-brand-700 transition-transform', open && 'rotate-180')}
         />
-        <h4 className="font-cairo font-bold text-brand-900">{title}</h4>
       </button>
       <div
         className={cn(
